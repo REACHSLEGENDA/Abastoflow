5c27d783faf446fa7dd8c2b28fb82d16cdd8d670
[dyad] <master9087s@gmail.com>
2025-10-01 03:13:38 -0600

    [dyad] Corregir error de sintaxis - wrote 1 file(s)

--- src/components/dashboard/ProfitStats.tsx
+++ src/components/dashboard/ProfitStats.tsx
@@ -71,7 +71,7 @@ export default function ProfitStats() {
               <Skeleton className="h-8 w-3/4" />
               <Skeleton className="h-4 w-1/2 mt-1" />
             </CardContent>
-          </Card>>
+          </Card>
         ))}
       </div>
     );
