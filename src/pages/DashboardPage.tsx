c2fecf2f3dd3ef79cfeedf1f11255f2c529be2a2
[dyad] <master9087s@gmail.com>
2025-10-01 03:00:02 -0600

    [dyad] Reordenar elementos del dashboard - wrote 1 file(s)

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -18,18 +18,18 @@ export default function DashboardPage() {
       <div className="flex flex-1 flex-col gap-4 md:gap-8">
         <DashboardStats />
         <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
-          <SalesOverTimeChart />
-          <div className="space-y-4">
-            <PaymentMethodChart />
-            <TopProductsChart />
-          </div>
-        </div>
-        <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
           <div className="xl:col-span-2">
             <RecentActivity />
           </div>
           <div>
             <LowStockProducts />
+          </div>
+        </div>
+        <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
+          <SalesOverTimeChart />
+          <div className="space-y-4">
+            <PaymentMethodChart />
+            <TopProductsChart />
           </div>
         </div>
       </div>
